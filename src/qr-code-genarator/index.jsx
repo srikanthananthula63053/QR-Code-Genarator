a949c4601cec95ad208018bfff3b80573a2df1a6
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-12-05 15:35:17 +0530

    npm i react-qr-code

---
+++ src/qr-code-genarator/index.jsx
@@ -0,0 +1,14 @@
+
+
+export default function QrGenarator(){
+ 
+    return(
+        <div>
+            <h1>QR CODE GENARATOR</h1>
+            <div>
+          <input type="text" name="" placeholder="enter the value"/>
+          <button></button>
+        </div>
+        </div>
+    );
+}
