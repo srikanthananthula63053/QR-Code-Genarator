446be78c94f4df84209b7fdbe7b068f639b001a8
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-12-06 19:41:56 +0530

    adding css styles

--- src/light-dark-mode/index.jsx
+++ src/light-dark-mode/index.jsx
@@ -1,13 +1,22 @@
 import useLocalStroage from "./useLocalStroage";
+//import "./styles.css";
+import "./theme.css";
 
 
 export default function LightDarkMode(){
-    const[theme,setTheme] = useLocalStroage('theme',"dark")
+    const[theme,setTheme] = useLocalStroage('theme',"dark");
+
+    function handleToggleTheme(){
+     setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+
+    console.log(theme)
+
     return(
         <div className="light-dark-mode">
             <div className="container">
         <p>Hello World!</p>
-        <button>change theme</button>
+        <button onClick={handleToggleTheme}>change theme</button>
         </div>
         </div>
     );
