4d2223f0213a9db3f803f0e8ef7549def4b14418
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-12-06 18:33:20 +0530

    creating button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import QrGenarator from './qr-code-genarator';
+import LightDarkMode from './light-dark-mode';
 
 function App() {
   return (
     <div className="App">
-      <QrGenarator/>
+      {/* <QrGenarator/> */}
+      <LightDarkMode/>
        
     </div>
   );
---
+++ src/light-dark-mode/index.jsx
@@ -0,0 +1,10 @@
+
+
+export default function LightDarkMode(){
+    return(
+        <div>
+        <h1>Hello World!</h1>
+        <button>theme</button>
+        </div>
+    );
+}
